49d0f308feb85ce2ebc3b175c258a9d721d13fb9
InKyoJeong <viapolar@naver.com>
2023-05-04 19:41:32 +0900

    chore: apps/ eslint 제거

--- apps/wanted/src/pages/index.tsx
+++ apps/wanted/src/pages/index.tsx
@@ -1,8 +1,8 @@
+import { sayHello } from '@wanted/lib';
+import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-import { sayHello } from '@wanted/lib';
 
 const inter = Inter({ subsets: ['latin'] });
 
